b93ab4011365e493bca8572ab2ed87536e94cdfe
Ivan Didyk <41524878+GermanSincovic@users.noreply.github.com>
2019-06-03 16:26:11 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -3,11 +3,10 @@
 	<head>
 		<title>MAESTRO Restaurant</title>
 		<meta charset="utf-8">
-		<link rel="stylesheet" type="text/css" href="http://maestro.loc/css/style.css">
 		<meta name="keywords" content="">
 		<meta name="description" content="">
-		<!-- <link rel="stylesheet" media="screen and (max-width:1024px)" type="text/css" href="css/mobile.css">
-		<link rel="stylesheet" media="screen and (min-width:1025px)" type="text/css" href="css/desktop.css"> -->
+		<link rel="stylesheet" media="screen and (max-width:1024px)" type="text/css" href="http://maestro.loc/css/mobile.css">
+		<link rel="stylesheet" media="screen and (min-width:1025px)" type="text/css" href="http://maestro.loc/css/style.css">
 	</head>
 	<body>
 		<header class="shadow">
@@ -27,14 +26,16 @@
 					<span></span>
 					<span></span>
 				</div>
-				<div class="burger-link-list">
-					<a href="/menu" class="nav-link">Наше меню</a>
-					<a href="/gallery" class="nav-link">Галерея</a>
-					<a href="/booking" class="nav-link">Бронирование столика</a>
-					<a href="/contacts" class="nav-link">Контакты</a>
-				</div>
 			</div>
 		</header>
+		<div class="burger-link-list">
+			<ul>
+				<li><a href="/menu" class="nav-link">Наше меню</a></li>
+				<li><a href="/gallery" class="nav-link">Галерея</a></li>
+				<li><a href="/booking" class="nav-link">Бронирование столика</a></li>
+				<li><a href="/contacts" class="nav-link">Контакты</a></li>
+			</ul>
+		</div>
 		<form id="search" onsubmit="location.href = '/search#' + this.query.value; return false;">
 			<input class="shadow-lowleft" type="text" name="query" placeholder="Поиск">
 			<input class="shadow-lowright" type="submit" value="&#10148;">
@@ -47,5 +48,19 @@
 			}
 		?>
 		<script type="text/javascript" src="http://maestro.loc/js/popup.js"></script>
+		<script type="text/javascript">
+			document.getElementsByClassName('burger-menu')[0].addEventListener('click',function(){
+				switch (this.className){
+					case 'burger-menu':{
+						this.className = 'burger-menu-open';
+						this.parentNode.nextElementSibling.className = 'burger-link-list-open'; 
+					}; break;
+					case 'burger-menu-open':{
+						this.className = 'burger-menu';
+						this.parentNode.nextElementSibling.className = 'burger-link-list'; 
+					}; break;
+				}
+			});
+		</script>
 	</body>
 </html>
